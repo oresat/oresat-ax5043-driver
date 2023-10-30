cb4a3ed7d9e7fe4f4ddc7333b8670eab583f8cfb
Theo Hill <Theo0x48@gmail.com>
2023-10-29 22:46:16 -0700

    Default runner to C3
    Most users will be trying to use this with the C3 board instead of the
    demo rpi board, so this reduces one point of friction.

--- runner.sh
+++ runner.sh
@@ -4,13 +4,13 @@ set -e
 bin=$1
 shift
 
-user="pi"
-host="raspberrypi.local"
-path=""
+#user="pi"
+#host="raspberrypi.local"
+#path=""
 
-#user="debian" # password: tmppwd
-#host="oresat-c3.local"
-#path="ax5043"
+user="debian" # password: tmppwd
+host="oresat-c3.local"
+path="ax5043"
 
 target="$user@$host"
 
